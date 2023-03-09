60a3b41ed8a8c5533b5d8c8cbfa130e25c70f769
vasya <vasyapirozhok2021@yandex.ru>
2023-03-09 20:32:44 +0300

    ready HW2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,25 @@
-from django.http import HttpResponse
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 
-
+OUTPUT_POSTS = 10
 def index(request):
-    return HttpResponse('Это главная страница')
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 def group_posts(request, slug):
-    return HttpResponse('Список постов')
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:OUTPUT_POSTS]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 
